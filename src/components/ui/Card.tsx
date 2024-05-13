3be3b590e72fd30ee6077ce025918f94c3490a15
Serhat Kaya <serhat_kaya@epam.com>
2024-05-13 15:12:56 +0300

    update card

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,9 +1,9 @@
 const Card = ({ icon, title, children }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
+    <div className="bg-white p-6 rounded-lg shadow-md border-2 border-accent/20">
       <div className="text-3xl mb-4">{icon}</div>
       <h3 className="text-placeholder text-xl font-bold mb-2">{title}</h3>
-      <p >{children}</p>
+      <p>{children}</p>
     </div>
   );
 };
