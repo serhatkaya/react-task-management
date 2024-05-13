c6b9bb069b6ebea33cafd103a51c1781c4989fc0
Serhat Kaya <serhat_kaya@epam.com>
2024-05-13 15:06:18 +0300

    card component added

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,11 @@
+const Card = ({ icon, title, children }) => {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md">
+      <div className="text-3xl mb-4">{icon}</div>
+      <h3 className="text-placeholder text-xl font-bold mb-2">{title}</h3>
+      <p >{children}</p>
+    </div>
+  );
+};
+
+export default Card;
