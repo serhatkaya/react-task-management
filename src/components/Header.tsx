994bac70c30a4808d6806682d238ceb20a98b8dc
Serhat Kaya <serhat_kaya@epam.com>
2024-05-13 14:36:49 +0300

    layout implemented

---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth.hook';
+import { cn } from '../utils/tw.util';
+import viteLogo from '/vite.svg';
+
+const Header = () => {
+  // For demonstration purposes, assume the user is logged in
+  const { isAuthenticated } = useAuth();
+  return (
+    <header className="bg-accent">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <div className="flex items-center">
+          <img
+            src={viteLogo}
+            className={cn('logo', 'w-10', 'h-10', 'mr-2')}
+            alt="Vite logo"
+          />
+          <span className="text-white font-bold text-lg">Your App Name</span>
+        </div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className="text-white hover:text-white">
+                Home
+              </Link>
+            </li>
+            {isAuthenticated() ? (
+              <>
+                <li>
+                  <Link to="/tasks" className="text-white hover:text-white">
+                    Tasks
+                  </Link>
+                </li>
+                <li>
+                  <button
+                    className="text-white hover:text-white"
+                    onClick={() => console.log('Logout')}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/register" className="text-white hover:text-white">
+                    Register
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className="text-white hover:text-white">
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,16 @@
 import { Outlet } from 'react-router-dom';
-
+import Header from './Header';
 function Layout() {
-  return <Outlet />;
+  return (
+    <>
+      <div className="min-h-screen">
+        <Header />
+        <div className="container mx-auto py-8">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Layout;
